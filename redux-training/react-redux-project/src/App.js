134d259da9f09544d502bbeecbffca30e8f61c1a
derektruong <minhduc0589@gmail.com>
2021-09-05 22:43:55 +0700

    complete secction 18

--- redux-training/react-redux-project/src/App.js
+++ redux-training/react-redux-project/src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+    const isAuthenticated = useSelector((state) => state.authReducer.isAuthenticated);
     return (
-        <>
+        <React.Fragment>
             <Header />
-            <Auth />
+            {isAuthenticated ? <UserProfile /> : <Auth />}
             <Counter />
-        </>
+        </React.Fragment>
     );
 }
 export default App;
--- redux-training/react-redux-project/src/components/Auth.js
+++ redux-training/react-redux-project/src/components/Auth.js
@@ -1,23 +1,52 @@
-import classes from './Auth.module.css';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
-  return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
-  );
+	// redux
+    const dispatch = useDispatch();
+
+	// state local
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const submitLoginHandler = (event) => {
+		event.preventDefault();
+		if(email.trim() === "" || password.trim() === "") {
+			return;
+		} else {
+			dispatch(authActions.login());
+		}
+
+		
+	}
+
+	const emailChangeHandler = (event) => {
+		setEmail(event.target.value);
+	}
+	
+	const passwordChangeHandler = (event) => {
+		setPassword(event.target.value);
+	}
+
+    return (
+        <main className={classes.auth}>
+            <section>
+                <form onSubmit={submitLoginHandler}>
+                    <div className={classes.control}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" value={email} onChange={emailChangeHandler}/>
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" value={password} onChange={passwordChangeHandler}/>
+                    </div>
+                    <button>Login</button>
+                </form>
+            </section>
+        </main>
+    );
 };
 
 export default Auth;
